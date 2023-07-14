73a71b0b18ff506e23e9654148f82041ba44fc3d
Murilo Barbosa <muriloab96@gmail.com>
2023-07-14 09:20:46 -0300

    Added app functionality and updated Code.html
    Finishing wriitng th eJavascript for the app page, and updated code.html witih the JAvascript code. Still needs to write function descriptions in code.html.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//Controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");//Make alert invisible when getValue is called
+
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+//Reverse the String
+//logic function
+function reverseString(userString) {
+
+    let revString=[];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >=0; index--) {
+        
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+//Display a message with the reversed string to the user
+//View function
+function displayString(revString) {
+
+    //Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
